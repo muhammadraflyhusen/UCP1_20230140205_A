e1aea5349760dae790888d8615ae282fe4fd372e
muhammadraflyhusen <raflyhusen311@gmail.com>
2025-05-13 21:06:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showAlert() {
+    alert("Pesan anda terkirim. Terima kasih telah mendaftar!");
+    return false;
+  }
+  
+  function showPopup() {
+    setTimeout(() => {
+      alert("🎉 Promo Spesial! Diskon 30% untuk pendaftar baru minggu ini!");
+    }, 2500);
+  }
+  
